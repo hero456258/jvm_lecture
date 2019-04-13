7179c4f53ccf781a0855ea517e793cbade7dde75
wanggang <wanggang@euka.cn>
2019-04-13 10:57:25 +0800

    类的加载、连接与初始化： 1.主动使用初始化一个类的子类 2.访问某个类或接口的静态变量，或则对该静态变量赋值

---
+++ src/main/java/com/mingqian/jvm/classloader/MyTest1.java
@@ -0,0 +1,50 @@
+package com.mingqian.jvm.classloader;
+
+/**
+ * Created by mingqian on 2019/4/13.
+ * 对于静态字段来说，只有直接定义了该字段的类才会被初始化
+ * 当一个类在初始化时，要求其父类全部都已经初始化完毕了
+ */
+public class MyTest1 {
+    public static void main(String[] args) {
+        System.out.println(MyChild1.str);
+        /**
+         * 运行结果：
+         * MyParent1 static block
+         * hello world
+         * 结果分析：
+         *      这里并没有初始化MyChild1因为str是定义在父类，所以没有主动使用MyChild1，
+         *      而是主动使用调用的父类，并初始化父类，
+         *      所以MyChild1并没有初始化并执行静态代码块
+         */
+
+        System.out.println(MyChild1.str2);
+        /**
+         * 运行结果：
+         * MyParent1 static block
+         * MyChild1 static block
+         * welcome
+         * 结果分析：
+         *    因为str2调用的MyChild1的静态变量，会主动使用MyChild1，
+         *    由于MyChild1继承了MyParent1所以会显示初始化MyParent1，
+         *    所以会执行MyParent1的静态代码块
+         *
+         *
+         */
+    }
+}
+
+class MyParent1{
+    public static String str = "hello world";
+    static {
+        System.out.println("MyParent1 static block");
+    }
+
+}
+
+class MyChild1 extends MyParent1{
+    public static String str2 = "welcome";
+    static {
+        System.out.println("MyChild1 static block");
+    }
+}
